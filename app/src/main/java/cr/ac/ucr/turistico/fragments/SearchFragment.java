84f1b4317b70b9ff0e5312bd396deb3daf5c4005
Alexis <axisfs12@gmail.com>
2020-09-26 19:10:31 -0600

    Firebase y Search
    Se crea la conexion con el firebase y se arregla el editText del fragment search

--- app/src/main/java/cr/ac/ucr/turistico/fragments/SearchFragment.java
+++ app/src/main/java/cr/ac/ucr/turistico/fragments/SearchFragment.java
@@ -14,6 +14,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
@@ -30,6 +31,8 @@ public class SearchFragment extends Fragment implements View.OnClickListener{
     private Button btnHills;
     private Button btnBeaches;
     private FrameLayout flHeader;
+
+    private EditText edSearch;
 
     /**
      * Constructor
@@ -74,12 +77,15 @@ public class SearchFragment extends Fragment implements View.OnClickListener{
         btnHills =  view.findViewById(R.id.btn_hill);
         btnBeaches =  view.findViewById(R.id.btn_beach);
 
+        edSearch = view.findViewById(R.id.ed_search);
+
         btnWaterfalls.setOnClickListener(this);
         btnHills.setOnClickListener(this);
         btnBeaches.setOnClickListener(this);
 
         return view;
     }
+
     /**
      * Metodo onAttach
      * @param context
