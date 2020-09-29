8b4e47452b93158aed8d45d4ed49b9ef05d4074c
Daniela Alarcón <daniela.alarcongamboa27@gmail.com>
2020-09-28 21:39:05 -0600

    SearchView customized implementation

--- app/src/main/java/cr/ac/ucr/turistico/fragments/SearchFragment.java
+++ app/src/main/java/cr/ac/ucr/turistico/fragments/SearchFragment.java
@@ -8,18 +8,27 @@
 package cr.ac.ucr.turistico.fragments;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Rect;
+import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.SearchView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 
 import cr.ac.ucr.turistico.R;
@@ -31,8 +40,8 @@ public class SearchFragment extends Fragment implements View.OnClickListener{
     private Button btnHills;
     private Button btnBeaches;
     private FrameLayout flHeader;
-
     private EditText edSearch;
+    private SearchView svSearch;
 
     /**
      * Constructor
@@ -76,12 +85,19 @@ public class SearchFragment extends Fragment implements View.OnClickListener{
         btnWaterfalls = view.findViewById(R.id.btn_waterfall);
         btnHills =  view.findViewById(R.id.btn_hill);
         btnBeaches =  view.findViewById(R.id.btn_beach);
+       /* edSearch = view.findViewById(R.id.ed_search);*/
+        svSearch = view.findViewById(R.id.sv_search);
 
-        edSearch = view.findViewById(R.id.ed_search);
 
         btnWaterfalls.setOnClickListener(this);
         btnHills.setOnClickListener(this);
         btnBeaches.setOnClickListener(this);
+        int id = svSearch.getContext().getResources().getIdentifier("android:id/search_src_text", null, null);
+        TextView textView = (TextView) svSearch.findViewById(id);
+        textView.setTextColor(Color.BLACK);
+        textView.setHintTextColor(Color.LTGRAY);
+
+        textView.setTypeface(ResourcesCompat.getFont(activity, R.font.quicksand_regular));
 
         return view;
     }
@@ -109,25 +125,45 @@ public class SearchFragment extends Fragment implements View.OnClickListener{
      * de cual boton sea seleccionado
      * @param view
      */
+
     @Override
     public void onClick(View view){
         switch (view.getId()){
-            case R.id.btn_waterfall:
-                changeToWaterFall();
-                flHeader.setBackgroundResource(R.drawable.river);
+            case R.id.btn_beach:
+                changeToBeaches();
+                flHeader.setBackgroundResource(R.drawable.manzanillo);
                 break;
             case R.id.btn_hill:
                 changeToHills();
                 flHeader.setBackgroundResource(R.drawable.pelado);
                 break;
-            case R.id.btn_beach:
-                changeToBeaches();
-                flHeader.setBackgroundResource(R.drawable.manzanillo);
+                case R.id.btn_waterfall:
+                changeToWaterFall();
+                flHeader.setBackgroundResource(R.drawable.river);
                 break;
             default:
                 break;
         }
     }
+
+   /* @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        String value = "";//any text you are pre-filling in the EditText
+
+        if (motionEvent.getAction() != MotionEvent.ACTION_DOWN) {
+            if (edSearch.isFocused()) {
+                edSearch.setHint("gi");
+            }
+            return true;
+        }
+        if(motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+            if (!edSearch.isFocused()) {
+                edSearch.setHint(value);
+            }
+            return false;
+        }
+        return false;
+    }*/
     /**
      * Metodo changeToWaterFall
      * Cambia el color de los botones y la tipografia
@@ -151,6 +187,7 @@ public class SearchFragment extends Fragment implements View.OnClickListener{
         btnHills.setTextColor(ContextCompat.getColor(activity.getApplicationContext(), R.color.white));
         btnBeaches.setBackground(ContextCompat.getDrawable(activity, R.drawable.text_area));
         btnBeaches.setTextColor(ContextCompat.getColor(activity.getApplicationContext(), R.color.black));
+
     }
     /**
      * Metodo changeToBeaches
@@ -163,6 +200,17 @@ public class SearchFragment extends Fragment implements View.OnClickListener{
         btnHills.setTextColor(ContextCompat.getColor(activity.getApplicationContext(), R.color.black));
         btnBeaches.setBackground(ContextCompat.getDrawable(activity, R.drawable.button));
         btnBeaches.setTextColor(ContextCompat.getColor(activity.getApplicationContext(), R.color.white));
-
     }
+
+/*
+
+    public void onFocusChange(View view, boolean b) {
+          if (b) {
+              edSearch.setHint("");
+          } else {
+              edSearch.setHint("Your hint");
+              edSearch.setText("hi");
+          }
+    }*/
 }
+
