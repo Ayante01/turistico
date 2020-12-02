4d00d71434d10bfb3b9efe055bf961e771b8f6d6
Daniela Alarcon <daniela.alarcon@ucr.ac.cr>
2020-12-02 13:13:55 -0600

    Profile and edit profile

--- app/src/main/java/cr/ac/ucr/turistico/EditProfile.java
+++ app/src/main/java/cr/ac/ucr/turistico/EditProfile.java
@@ -34,7 +34,7 @@ import java.util.HashMap;
 import cr.ac.ucr.turistico.fragments.ProfileFragment;
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class EditProfile extends AppCompatActivity {
+public class EditProfile extends AppCompatActivity implements View.OnClickListener {
 
     private CircleImageView profileImageView;
     private Button cancelButton;
@@ -71,36 +71,19 @@ public class EditProfile extends AppCompatActivity {
         saveButton = findViewById(R.id.btn_save);
         changeProfile = findViewById(R.id.change_profile_btn);
 
-        cancelButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(EditProfile.this, ProfileFragment.class));
-            }
-        });
-
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                uploadProfileInfo();
-            }
-        });
-
-        changeProfile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                CropImage.activity().setAspectRatio(1,1).start(EditProfile.this);
-            }
-        });
+        cancelButton.setOnClickListener(this);
+        saveButton.setOnClickListener(this);
+        changeProfile.setOnClickListener(this);
 
         getUserInfo();
     }
 
-    private void getUserInfo(){
+    private void getUserInfo() {
         databaseReference.child(aAuth.getCurrentUser().getUid()).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(snapshot.exists() && snapshot.getChildrenCount() > 0){
-                    if(snapshot.hasChild("imgPerfil")){
+                if (snapshot.exists() && snapshot.getChildrenCount() > 0) {
+                    if (snapshot.hasChild("imgPerfil")) {
                         String image = snapshot.child("imgPerfil").getValue().toString();
                         Picasso.get().load(image).into(profileImageView);
                     }
@@ -118,17 +101,17 @@ public class EditProfile extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK && data != null){
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
             CropImage.ActivityResult result = CropImage.getActivityResult(data);
             imageUri = result.getUri();
 
             profileImageView.setImageURI(imageUri);
-        }else{
+        } else {
             Toast.makeText(this, "Error, Try again", Toast.LENGTH_SHORT).show();
         }
     }
 
-    private void  uploadProfileInfo(){
+    private void uploadProfileInfo() {
         final ProgressDialog progressDialog = new ProgressDialog(this);
         newName = editName.getText().toString().trim();
         newLastName = editLastName.getText().toString().trim();
@@ -136,16 +119,15 @@ public class EditProfile extends AppCompatActivity {
         progressDialog.setMessage("Please wait while your data is being setting");
         progressDialog.show();
 
-        if(imageUri != null){
+        if (imageUri != null) {
             final StorageReference fileRef = storageProfilePicsRef.child("fotosPerfil")
-                    .child(aAuth.getCurrentUser().getUid()+".jpg");
+                    .child(aAuth.getCurrentUser().getUid() + ".jpg");
             uploadTask = fileRef.putFile(imageUri);
 
             uploadTask.continueWithTask(new Continuation() {
                 @Override
                 public Object then(@NonNull Task task) throws Exception {
-                    if(!task.isSuccessful())
-                    {
+                    if (!task.isSuccessful()) {
                         throw task.getException();
                     }
                     return fileRef.getDownloadUrl();
@@ -153,7 +135,7 @@ public class EditProfile extends AppCompatActivity {
             }).addOnCompleteListener(new OnCompleteListener<Uri>() {
                 @Override
                 public void onComplete(@NonNull Task<Uri> task) {
-                    if(task.isSuccessful()){
+                    if (task.isSuccessful()) {
                         Uri downloadUrl = task.getResult();
                         myUri = downloadUrl.toString();
 
@@ -166,9 +148,27 @@ public class EditProfile extends AppCompatActivity {
                     }
                 }
             });
-        }else {
+        } else {
             progressDialog.dismiss();
             Toast.makeText(this, "Image not selected", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_cancel:
+                Intent cancel = new Intent(EditProfile.this, ProfileFragment.class);
+                startActivity(cancel);
+                break;
+            case R.id.btn_save:
+                uploadProfileInfo();
+                Intent save = new Intent(EditProfile.this, ProfileFragment.class);
+                startActivity(save);
+                break;
+            case R.id.change_profile_btn:
+                CropImage.activity().setAspectRatio(1, 1).start(EditProfile.this);
+                break;
         }
     }
 }
